6a8adc0940d0445fbea1e05cfc321de71b981339
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2023-10-21 21:21:18 +0530

    testing if file sending works

--- app/page.tsx
+++ app/page.tsx
@@ -13,12 +13,22 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
-import { Rocket, Files, PlugZap } from "lucide-react"
+import { Rocket, Files, PlugZap, ChevronsUpDown } from "lucide-react"
 import { Input } from "@/components/ui/input";
 import { useToast } from "@/components/ui/use-toast"
 import { Label } from "@/components/ui/label";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+import { Separator } from "@/components/ui/separator"
+import Link from "next/link";
 
-
+interface fileData {
+  fileName: string;
+  fileUrl: string;
+}
 
 export default function Home() {
   const { toast } = useToast()
@@ -28,20 +38,37 @@ export default function Home() {
   }
 
   const [peer, setPeer] = useState<Peer | null>(null);
-  const [id, setId] = useState<string | null>(null);
+  const [url, setUrl] = useState<string | undefined>(undefined);
   const [isConnected, setIsConnected] = useState<boolean>(false);
   const [remoteId, setRemoteId] = useState<string | null>(null);
   const [conn, setConn] = useState<any | null>(null);
-  // const [remotePeer, setRemotePeer] = useState<Peer | null>(null);
+  const [files, setFiles] = useState<fileData[]>([]);
+  const [name, setName] = useState<string | null>(null);
+
+  const [remoteListIsExpanded, setRemoteListIsExpanded] = useState(false)
 
   useEffect(() => {
     if (peer) {
       peer.on("connection", (conn) => {
         conn.on("open", () => {
+          const remoteId = conn.peer;
+          setRemoteId(remoteId);
+          setConn(conn);
+          setIsConnected(true);
+          toast({ title: `Connected to ${remoteId}`, description: "You can now share files!" });
         })
         conn.on("data", (data: any) => {
           console.log("receiving...")
-          toast({ title: data})
+          console.log(files)
+          if (data.fileName && data.fileData) {
+            const file = new Blob([data.fileData], { type: data.fileType });
+            const fileName = data.fileName;
+            const fileUrl = URL.createObjectURL(file);
+            setFiles([...files, { fileName, fileUrl }]);
+          }
+
+
+          // toast({ title: data})
         });
       });
     }
@@ -57,16 +84,26 @@ export default function Home() {
       const conn_ = peer.connect(remoteId!);
       setConn(conn_);
       setIsConnected(true);
-      conn_.on("open", () => {
-        toast({ title: `Connected to ${remoteId}`, description: "You can now share files!" });
-        conn_.send(`${peer.id} says hi!`);
-      });
+    }
+  }
+
+  const sendFiles = () => {
+    if (conn) {
+      const files = document.getElementById("files") as HTMLInputElement;
+      if (files.files) {
+        for (let i = 0; i < files.files.length; i++) {
+          const fileData = files.files[i];
+          const fileName = fileData.name;
+          const fileType = fileData.type;
+          conn.send({ fileData, fileName, fileType });
+        }
+      }
     }
   }
 
   return (
-    <main className="flex min-h-screen h-screen flex-col items-center justify-center p-10 bg-lime-200">
-      <Card className="w-full md:w-3/4 lg:w-1/2 h-full md:h-3/4 lg:h-1/2 flex flex-col items-center justify-center">
+    <main className="flex min-h-screen h-screen items-center justify-center p-10 bg-lime-200">
+      <Card className="w-full md:w-3/4 lg:w-1/2 h-full md:min-h-3/4 lg:min-h-1/2 flex flex-col items-center justify-center p-4">
         <CardHeader className="text-center">
           <CardTitle className="text-4xl">Oreweb</CardTitle>
           <CardDescription>Share Your Files With Ease!</CardDescription>
@@ -85,28 +122,57 @@ export default function Home() {
                 <Label htmlFor="remoteId">Connect to Remote</Label>
                 <div className="flex items-center justify-between">
                   <Input id="remoteId" type="text" maxLength={5} placeholder="Remote ID" value={remoteId!} onChange={(e) => setRemoteId(e.target.value)} />
-                  <Button className="ml-4" size={"sm"} onClick={() => connectToRemote()}>
+                  <Button className="ml-4" size={"sm"} disabled={!remoteId || (remoteId?.length != 5)} onClick={() => connectToRemote()}>
                     <PlugZap className="h-4 w-4" />
                   </Button>
                 </div>
               </>
             ) : (
               <>
-                <p>Connected to <span className="font-semibold">{remoteId}</span></p>
-
-                <div className="grid w-full max-w-sm items-center gap-1.5 pt-6">
+                <p className="text-sm">Connected to <span className="font-semibold">{remoteId}</span></p>
+                <Collapsible
+                  open={remoteListIsExpanded}
+                  onOpenChange={setRemoteListIsExpanded}
+                  className="w-[350px] space-y-2"
+                >
+                  <div className="flex items-center justify-between space-x-4">
+                    <h4 className="text-sm font-semibold">
+                      Received 10 Files
+                    </h4>
+                    <CollapsibleTrigger asChild>
+                      <Button variant="ghost" size="sm" className="w-9 p-0">
+                        <ChevronsUpDown className="h-4 w-4" />
+                        <span className="sr-only">Toggle</span>
+                      </Button>
+                    </CollapsibleTrigger>
+                  </div>
+                  {(files.length > 0) ? (
+                    <CollapsibleContent className="space-y-2">
+                      {files.map((file) => (
+                        <a className="rounded-md border px-4 py-3 font-mono text-sm w-full" href={file.fileUrl}>
+                          { file.fileName }
+                        </a>
+                      ))}
+                    </CollapsibleContent>
+                  ) : (
+                    <div className="rounded-md border px-4 py-2 font-mono text-sm">
+                      No files received yet!
+                    </div>
+                  ) }
+                  
+                </Collapsible>
+                <Separator className="my-6" />
+                <div className="grid w-full max-w-sm items-center gap-1.5">
                   <Label htmlFor="files">Share files</Label>
                   <Input id="files" type="file" multiple />
                   {/* send button */}
-                  <Button size={"sm"} onClick={() => {}}>
+                  <Button size={"sm"} onClick={() => sendFiles()}>
                     Send Selected Files
                     <Rocket className="ml-2 h-4 w-4" />
                   </Button>
                 </div>
               </>
             )}
-
-            
           </CardContent>
         ) : (
             <Button onClick={() => connect()}><Rocket className="mr-2 h-4 w-4" />Connect to Share</Button>
